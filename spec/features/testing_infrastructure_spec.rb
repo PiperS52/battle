e789aa68d924798e14972dc55cab9582cdfa4a48
calavell <calavell@tcd.ie>
2020-12-09 15:53:46 +0000

    entering players complete (another feature test added)

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,16 @@
-feature 'Testing infrastructure' do
-  scenario 'can run app and check page content' do
+# feature 'Testing infrastructure' do
+#   scenario 'can run app and check page content' do
+#     visit('/')
+#     expect(page).to have_content "Testing infrastructure working!"
+#   end
+# end
+
+feature 'Entering names in form' do
+  scenario 'two players can enter their names and see them' do
     visit('/')
-    expect(page).to have_content "Testing infrastructure working!"
+    fill_in :player_1_name, with: "Cathal"
+    fill_in :player_2_name, with: "Simon"
+    click_button("Submit")
+    expect(page).to have_content "Cathal vs. Simon"
   end
-end 
+end
